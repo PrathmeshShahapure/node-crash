78b76a20789a9b58368bc69043b46690d06deb9a
PrathmeshShahapure <prathmeshshahapure28@gmail.com>
2026-08-05 15:50:13 +0530

    express: 5 Error handling

--- 03-express/01-server.js
+++ 03-express/01-server.js
@@ -1,13 +1,19 @@
 import express from "express"
-
+import errorHandler from "./05-error.js";
 const app = express();
 
 app.get("/", (req, res) => { 
     res.send("Hello !!!")
 })
 
+app.get("/error", (req, res) => {
+  throw new Error("Something went wrong!");
+});
+
 app.listen(3000, () => { 
     console.log("your server is running on port 3000");
 })
+
+app.use(errorHandler)
 
 export default app
---
+++ 03-express/05-error.js
@@ -0,0 +1,10 @@
+// middleware/error.js
+
+export default function errorHandler(err, req, res, next) {
+  console.error(err);
+
+  res.status(500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+}
